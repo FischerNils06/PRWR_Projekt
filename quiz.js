53a8640dba11cc6f6d072f749d03a75316e41253
Nils Fischer <fischern@bzz.ch>
2022-12-04 21:49:32 +0100

    Quiz beendet und Startseite fortgeführt

--- quiz.js
+++ quiz.js
@@ -1,21 +1,22 @@
 const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const questioncontainerElement = document .getElementById('questioncontainerquiz')
-const questionElement = document.getElementById('questiona')
-const answerButtonElements = document.getElementById('answerbuttonsa')
-
+const questioncontainerElement = document.getElementById('questioncontainerquiz')
+const questionElement = document.getElementById('question')
+const answerButtonElements = document.getElementById('answerbuttons')
+let score2 = 0;
 let shuffledQuestions, currentQuestinIndex
 
-startButton .addEventListener('click', startGame)
-nextButton.addEventListener('click', () =>{
-    currentQuestinIndex++
-    setNextQuestion()
+startButton.addEventListener('click', startGame)
+answerButtonElements.addEventListener('click', () =>{
+    currentQuestinIndex += 1;
+    setTimeout(setNextQuestion, 1000)
+
 })
 
 function startGame() {
 startButton.classList.add('hide')
-shuffledQuestions = questions.sort(() => 1)
+shuffledQuestions = questions.sort(() => Math.random() - .5)
 currentQuestinIndex = 0
+score2 = 0;
 questioncontainerElement .classList.remove('hide')
 setNextQuestion()
 }
@@ -33,6 +34,8 @@ question.answers.forEach(answer =>{
     button.classList.add('btn2')
     if (answer.correct) {
         button.dataset.correct = answer.correct
+        button.classList.add('correct-btn')
+       
     }
     button.addEventListener('click' , selectAnswer)
     answerButtonElements.appendChild(button)
@@ -41,7 +44,7 @@ question.answers.forEach(answer =>{
 }
 
 function resetState() {
-    nextButton.classList.add('hide')
+    
     clearStatusClass(document.body)
     while (answerButtonElements.firstChild) {
         answerButtonElements.removeChild
@@ -58,31 +61,47 @@ Array.from(answerButtonElements.children).forEach(button =>{
     setStatusClass(button, button.dataset.correct)
 })
 
-    nextButton.classList.remove('hide')
-
-    if (shuffledQuestions.lenght > currentQuestinIndex + 1) {
-        nextbutton.classList.remove('hide')
+    
+    // if it is the last question, then show the score
+    
+    
+    if (shuffledQuestions.lenght < currentQuestinIndex + 1) {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+        questionElement.innerText = questions.question
     } else{
         startButton.innerText = 'Restart'
         startButton.classList.remove('hide')
+        let score3 = score2 - currentQuestinIndex -1;
+        questionElement.innerText = 'Dein Score ist ' + score3 + ' von ' + questions.length;
+        
+        
     }
     
 
 }  
 
 function setStatusClass(element, correct) {
+    
     clearStatusClass(element)
-    if (correct){
+        
+    
+    if (correct) {
         element.classList.add('correct')
+        score2 += 1;
+
     } else{
         element.classList.add('wrong')
     }
+    
 }
 
 function clearStatusClass(element) {
     element.classList.remove('correct')
     element.classList.remove('wrong')
 }
+
+
 
 const questions = [
     {
@@ -96,16 +115,32 @@ const questions = [
         ]
    
     }, {
-        question: 'Von welcher kommt Nintendo',
+        question: 'Von welcher Stadt kommt Nintendo',
         answers: [
             { text: 'Tokio',correct: false},
             { text:'Osaka',correct: false},
             { text: 'Kyoto',correct: true},
             { text: 'Nagoya',correct:false}
         ]
+    }, {
+        question: 'Wie viele Mitarbeiter hat Nintendo?',
+        answers: [
+            { text: '1000',correct: false},
+            { text:'2000',correct: false},
+            { text: '3000',correct: false},
+            { text: '4000',correct:true}
+        ]
+    }, {
+        question: 'Wie viele Spiele hat Nintendo entwickelt?',
+        answers: [
+            { text: '100',correct: false},
+            { text:'200',correct: false},
+            { text: '300',correct: true},
+            { text: '400',correct:false}
+        ]
     }
 
 
  
   
-]
+];
