abd5fa33ae7c27e4be6fa17fd279d3cdf060df77
Nils Fischer <fischern@bzz.ch>
2022-12-08 21:49:33 +0100

    Website Finished

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-table_list = document.getElementById("table_list");
-table = document.getElementById("table");
 
-// change table into list if the screen is too small
-if (window.innerWidth < 600) {
-    table_list.style.display = "block";
-    table.style.display = "none";
-}
 
