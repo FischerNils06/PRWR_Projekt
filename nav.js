fc5283f649ef939cdc7061482b8679a407f3276d
Nils Fischer <fischern@bzz.ch>
2022-12-09 17:54:44 +0100

    webseite beendet

--- nav.js
+++ nav.js
@@ -1,20 +1,15 @@
-button = document.getElementById('btn');
-button4 = document.getElementById('btn4');
+const object  = document.querySelectorAll('object');
+const bttn = document.getElementsByClassName('bttn');
+// create a variable for the screen width
+var screenWidth = window.innerWidth;
+// create a variable for the screen height
+var screenHeight = window.innerHeight;
 
-// change button color to white if your on the page it links to
-if (window.location.pathname == "https://fischernils06.github.io/PRWR_Projekt/contact.html" || window.location.pathname == "127.0.0.1:5500/contact.html") {
-    button.classList.add('active');
-}
-else if (window.location.pathname == "index.html") {
-    //add active to button after loading next page
-    button.classList.add('active');
+if  (screenWidth < 1000) {
+    for (let i = 0; i < bttn.length; i++) {
+        bttn[i].addEventListener('click', () => {
+            object.classList.add('hide');
+        });
     
-} else if (window.location.pathname == "01_vor_videospielen.html") {
-    button4.classList.add('active');
 }
-
-
-
-
-
-
+}
